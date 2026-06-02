647872a3700e4454cf82542a87201aa65315cec9
burakzdmr01 <burakyusufozdemir@hotmail.com>
2026-06-02 07:31:39 +0300

    image update#5

--- utopia/settings.py
+++ utopia/settings.py
@@ -214,4 +214,11 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    'default': {
+        'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
+    },
+    'static': {
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+    },
+}
