85a71dc8e9d12a88e9624640c903968a30ebc24a
burakzdmr01 <burakyusufozdemir@hotmail.com>
2026-06-02 07:34:39 +0300

    image update#6

--- utopia/settings.py
+++ utopia/settings.py
@@ -218,7 +218,7 @@ STORAGES = {
     'default': {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
-    'static': {
+    'staticfiles': {
         'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     },
 }
